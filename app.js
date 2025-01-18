3981f5e34276b35eaf3aeb59389677a04530e3f5
virajpwdn <virajpatwardhan6@gmail.com>
2025-01-18 14:45:14 +0530

    exploring rotues

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
 const express = require("express");
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("This hello page guys");
+
+app.get("/ab?c", (req, res)=>{
+    res.send({userName: "kartik", lastName: "Aaryan"});
+})
+
+app.get("/ab+c", (req, res)=>{
+    res.send({userName: "kartik", lastName: "Aaryan"});
+})
+
+app.get("/ab*c", (req, res)=>{
+    res.send({userName: "kartik", lastName: "Aaryan"});
+})
+
+app.get("/a(bc)?d", (req, res)=>{
+    res.send({userName: "kartik", lastName: "Aaryan"});
+})
+
+app.get("/user:userID", (req, res)=>{
+    console.log(req.params);
+    res.send({userName: "kartik", lastName: "Aaryan"});
+})
+
+app.get(/.*fly$/, (req, res)=>{
+    // console.log(req.query);
+    res.send({userName: "kartik", lastName: "Aaryan"});
+})
+
+app.post("/user", (req,res)=>{
+    // logic to change will come here
+    res.send("Data Successfully Created");
+})
+
+app.patch("/user", (req, res)=>{
+    res.send("Data updated successfully");
+})
+
+app.delete("/user", (req,res)=>{
+    res.send("Data deleted successfully");
+})
+
+app.use("/test", (req, res) => {
+  res.send("This is a test route");
 });
-app.use((req, res) => {
-  res.send("Welcome to the server friends");
-});
+
 
 
 app.listen(3000, () => {
