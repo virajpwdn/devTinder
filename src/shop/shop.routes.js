9309081869e89dbcb74d507e6ea45ce5bcd24950
virajpwdn <virajpatwardhan6@gmail.com>
2025-03-09 15:05:48 +0530

    created first order

--- src/shop/shop.routes.js
+++ src/shop/shop.routes.js
@@ -1,8 +1,27 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const shopRouter = Router();
+const { authenticate } = require("../middlewares/auth");
+const razorpayOrder = require("../utils/razorpay");
 
-shopRouter.get("/premium", (req,res)=>{
-    res.send("Hello world this is premium page")
-})
+shopRouter.post("/payment/create", authenticate, async (req, res) => {
+  try {
+    const order = await razorpayOrder.orders.create({
+      amount: 10000,
+      currency: "INR",
+      receipt: "receipt#1",
+      partial_payment: false,
+      notes: {
+        firstName: req.user.firstName,
+        lastName: req.user.lastName,
+        email: req.user.emailId,
+        membershipType: "gold"
+      },
+    });
+    console.log(order);
+    res.status(201).json({order})
+  } catch (error) {
+    res.status(400).json({message: error.message})
+  }
+});
 
 module.exports = shopRouter;
