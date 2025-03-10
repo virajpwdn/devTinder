c81cef71e66b4c474bd9f0bd0987ea2fe87ddf95
virajpwdn <virajpatwardhan6@gmail.com>
2025-03-10 18:52:57 +0530

    bug fix with frontend

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,12 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const createRoomId = ({ targetId, userId }) => {
+  return crypto
+    .createHash("sha256")
+    .update([targetId, userId].sort().join("_"))
+    .digest("hex");
+};
 
 const initializeSocket = (server) => {
   const io = socket(server, {
@@ -8,17 +16,18 @@ const initializeSocket = (server) => {
   io.on("connection", (socket) => {
     console.log("Socket is Connected");
 
-    socket.on("joinChat", ({targetId, userId}) => {
-        const room = [targetId, userId].sort().join("_");
-        socket.join(room);
-        // console.log("Joining Room:", room);
+    socket.on("joinChat", ({ targetId, userId }) => {
+      const room = createRoomId(targetId, userId);
+      console.log(room);
+      socket.join(room);
+      // console.log("Joining Room:", room);
     });
 
-    socket.on("sendMessage", ({firstName, userId, targetId, newMessage}) => {
-        // console.log(firstName, userId, targetId, newMessage);
-        const room = [targetId, userId].sort().join("_");
-        console.log(room);
-        io.to(room).emit("messageReceived", {newMessage});
+    socket.on("sendMessage", ({ firstName, userId, targetId, newMessage }) => {
+      // console.log(firstName, userId, targetId, newMessage);
+      const room = createRoomId(targetId, userId);
+      console.log(room);
+      io.to(room).emit("messageReceived", { firstName, newMessage });
     });
 
     socket.on("disconnect", () => {});
