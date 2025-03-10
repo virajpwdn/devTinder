d4b057faf704c5b332acfbf47278206a0f835a34
virajpwdn <virajpatwardhan6@gmail.com>
2025-03-10 14:37:20 +0530

    message receiving on both rooms

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -8,11 +8,18 @@ const initializeSocket = (server) => {
   io.on("connection", (socket) => {
     console.log("Socket is Connected");
 
-    socket.on("joinChat", ({check}) => {
-        console.log(check);
+    socket.on("joinChat", ({targetId, userId}) => {
+        const room = [targetId, userId].sort().join("_");
+        socket.join(room);
+        // console.log("Joining Room:", room);
     });
 
-    socket.on("sendMessage", () => {});
+    socket.on("sendMessage", ({firstName, userId, targetId, newMessage}) => {
+        // console.log(firstName, userId, targetId, newMessage);
+        const room = [targetId, userId].sort().join("_");
+        console.log(room);
+        io.to(room).emit("messageReceived", {newMessage});
+    });
 
     socket.on("disconnect", () => {});
   });
